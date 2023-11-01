ecb5000a29414f09f903529de88c7d0d5898348d
Ruan Finamor <ruanfinamor@gmail.com>
2023-11-01 11:44:49 -0300

    logos e página de login

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,23 +1,52 @@
 import React from 'react';
-import { Image, ImageBackground, StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native';
+import { Image, ImageBackground, StyleSheet, View, Text, Button, TouchableOpacity, SafeAreaView } from 'react-native';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 function WelcomeScreen(props) {
-  return (  
+  return (
+    
       <ImageBackground
         style={styles.background}
-        source={require("../assets/pexels-elvis-2528116.jpg")}
-        >
+        source={require("../assets/background_image.jpg")}
+      >
+        <SafeAreaView style={styles.container}>
           <View style={styles.logoContainer}>
-            <Image style={styles.logo} source={require("../assets/bird_2.png")} />
-            <Text>My first React Native App</Text>
+            <Image style={styles.logo} source={require("../assets/react-native-logo.png")} />
+            <Text style={styles.firstText}>My first React Native App</Text>
           </View>
-          <View style={styles.loginButton}>
-            <TouchableOpacity onPress={() => console.log("Image tapped")}>
-              <Text style={styles.loginButtonTitle}>Login</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.registerButton}></View>
-        </ImageBackground>   
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginButtonTitle}>Login</Text>
+            <MaterialIcons name="arrow-forward-ios" size={22} color="#fff" />
+          </TouchableOpacity>
+        </SafeAreaView>
+      </ImageBackground>
+    
+  );
+}
+
+const Home = () => {
+  return(
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
+
+function App () {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={WelcomeScreen} name="Welcome Screen" />
+        <Stack.Screen component={Home} name="Home" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -25,33 +54,48 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
-    alignItems: "center",
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
+  loginButtonTitle: {
+    color: "#fff",
+    fontWeight: 'bold',
+    fontSize: 18,
   },
   loginButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#fc5c65",
-  },
-  loginButtonTitle: {
-    color: "#fff",       
-    textAlign: "center",
-    paddingTop: 25,
-    fontFamily: 'Cochin',
-  },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "#4ecdc4",
+    backgroundColor: "#da4526",
+    padding: 20,
+    width: "95%",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderRadius: 5,
+    marginTop: 720,
+
   },
   logo: {
-    width: 100,
-    height: 200,
+    width: 120,
+    height: 120,
+    marginTop: 20,
   },
   logoContainer: {
     position: 'absolute',
-    top: 10,    
+    top: 10,
     alignItems: "center",
+    padding: 20,
+    paddingRight: 6,
+  },
+  firstText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#da4526',
+    textShadowColor: '#fff', textShadowOffset: { width: 0.75, height: 0.75 }, textShadowRadius: 1
+
   },
 })
 
